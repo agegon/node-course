6eb58c5987124f0933f077f5c9c260378bfb7f5a
agegon <lpchupa@gmail.com>
2020-03-10 17:44:33 +0300

    l43: Add cart method to add course

--- models/user.js
+++ models/user.js
@@ -25,4 +25,24 @@ const user = new Schema({
   }
 })
 
+user.methods.addToCart = function(course) {
+  if (!course) return;
+
+  const items = [...this.cart.items];
+  const idx = items.findIndex(item => item.course.toString() === course._id.toString());
+
+  if (idx >= 0) {
+    items[idx].count++;
+  } else {
+    items.push({
+      course: course._id,
+      count: 1
+    })
+  }
+
+  console.log(items, course);
+  this.cart.items = items;
+  return this.save();
+}
+
 module.exports = model('User', user);
--- routes/card.js
+++ routes/card.js
@@ -1,13 +1,12 @@
 const { Router } = require('express');
 const router = Router();
 
-const Card = require('../models/card');
 const Course = require('../models/course');
 
 router.post('/add', async (req, res) => {
-  const course = await Course.getById(req.body.id);
-  await Card.add(course);
-  res.redirect('/courses');
+  const course = await Course.findById(req.body.id);
+  await req.user.addToCart(course);
+  res.redirect('/card');
 })
 
 router.delete('/:id', async (req, res) => {
